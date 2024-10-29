4e3058ea5af741ecc7ac38792d6ffe00b17ff38e
evanmichaelstrand <evanmstrand@gmail.com>
2024-10-29 14:14:36 -0600

    pushing after upgrading packages

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -23,7 +23,7 @@ export function Form() {
   useEffect(() => {
     if (state.succeeded) {
       toast.success('Email successfully sent!', {
-        position: toast.POSITION.BOTTOM_LEFT,
+        //position: toast.POSITION.BOTTOM_LEFT,
         pauseOnFocusLoss: false,
         closeOnClick: true,
         hideProgressBar: false,
