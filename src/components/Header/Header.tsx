67f1fc03f751172aaaf0c7b396107da42be3936a
Strand <evanmstrand@gmail.com>
2025-02-21 15:03:38 -0700

    updating resume to feb 2025

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
 
-import resume from '../../assets/Resume2024.pdf'
+import resume from '../../assets/Resume2025.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
 
